80d990c609cf0803f9e7a2123f28a761ea073734
vivek singh <vikky30singh@gmail.com>
2021-04-14 15:49:38 +0530

    added floatingActionButton for toolbar and configured webpack for style loader

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Working With TypeScript .');
+import 'materialize-css/dist/css/materialize.min.css';
+import { FloatingActionButton } from 'materialize-css';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    FloatingActionButton.init(elems, {
+        direction: 'left',
+        hoverEnabled: false
+    });
+});
+
+console.log('Working With TypeScript.');
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
                     path.resolve(__dirname, 'src')
                 ],
                 use: 'ts-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
             }
         ]
     }
