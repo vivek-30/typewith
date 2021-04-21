c27bf605b57386d5a88d85efceaff0ad423b1154
vivek singh <vikky30singh@gmail.com>
2021-04-22 00:43:31 +0530

    minor script changes

--- server/server.js
+++ server/server.js
@@ -7,17 +7,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin: 'http://127.0.0.1:3000'
+        origin: 'http://localhost:3000'
     }
 ));
 
 const io = socket(server, {
     cors: {
-        origin: 'http://127.0.0.1:3000',
+        origin: 'http://localhost:3000',
         methods: [ 'GET', 'POST' ]
     }
 });
-
 
 server.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
