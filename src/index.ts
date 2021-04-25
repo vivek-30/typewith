180f33506ee30863bcba6a839e524b3c547c06e7
vivek singh <vikky30singh@gmail.com>
2021-04-25 22:28:36 +0530

    revisited old native way

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import 'materialize-css/dist/css/materialize.min.css';
 import { FloatingActionButton } from 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import './index.css'
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
