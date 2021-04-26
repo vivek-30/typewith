ee3f6ef30028d447ad4e480894bc26522e968175
vivek singh <vikky30singh@gmail.com>
2021-04-26 11:28:23 +0530

    created seperate file to handle DOM Elements

---
+++ src/Elements.ts
@@ -0,0 +1,6 @@
+export const ToolBarButton = document.querySelector('.fixed-action-btn') as HTMLDivElement;
+export const drawerBtn = document.querySelector('#drawer-btn') as HTMLButtonElement;
+export const sideBar = document.querySelector('#sideBar') as HTMLDivElement;
+export const main = document.querySelector('main')!;
+export const drawerBtnIcon = drawerBtn.querySelector('i') as HTMLElement;
+export const toolBarBtn = drawerBtn.querySelector('#toolBarBtn') as HTMLButtonElement;
