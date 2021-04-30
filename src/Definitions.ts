af27da77d1e6c5b52acade4e62bea1e74100e940
vivek singh <vikky30singh@gmail.com>
2021-04-30 23:58:19 +0530

    added new "WorkType" str.

--- src/Definitions.ts
+++ src/Definitions.ts
@@ -1,3 +1,10 @@
 // Type Definitions.
 export type Tool = 'save' | 'add' | 'color_lens' | 'text_format' | 'delete' | 'folder_open';
 export type DrawerIconType = 'chevron_left' | 'chevron_right';
+
+// Interface Definitions.
+export interface WorkType {
+  title: string;
+  content: string;
+  color: string;
+}
