77cf8f0e58e95ebef9f650a46b9984125cc61e0b
vivek singh <vikky30singh@gmail.com>
2021-05-04 23:25:47 +0530

    handled major events like "provide users list, share content and add/remove a user" in backend

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const socket = require('socket.io');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
+var users = [];
+var filteredList = [];
 
 app.use(cors(
   {
@@ -20,8 +22,27 @@ const io = socket(server, {
 
 io.on('connection', (socket) => {
 
+  const { id: ID } = socket;
+
+  socket.on('join-me', (name) => {
+    let newUser = { id: ID, name };
+    users.push(newUser);
+    socket.broadcast.emit('new-user', newUser);
+  });
+
   socket.on('writing', (content) => {
     socket.broadcast.emit('writing', content);
+  });
+
+  socket.on('get-users-list', () => {
+    filteredList = users.filter(({ id }) => id !== ID);
+    io.to(ID).emit('update-users-list', filteredList);
+  });
+
+  socket.on('disconnect', () => {
+    let index = users.findIndex(({ id }) => id === ID);
+    let person = users.splice(index, 1);
+    socket.broadcast.emit('remove-user', ...person);
   });
 
 });
